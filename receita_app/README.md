27d81b7cd480d2147a40ad86d09d6a2c6e69cf7a
Bruno Campagnol <35274635+brunounky@users.noreply.github.com>
2025-07-16 19:34:06 -0300

    Create new project

---
+++ receita_app/README.md
@@ -0,0 +1,3 @@
+# receita_app
+
+A new Flutter project.
---
+++ receita_app/android/app/src/main/kotlin/com/example/receita_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.receita_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
